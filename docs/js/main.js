07f8b89d986ab9357908e46de409a7be1f199b49
alysonnakamura <howdy@alysonnakamura.com>
2017-05-29 11:03:58 -0400

    Update main.js

--- docs/js/main.js
+++ docs/js/main.js
@@ -1,12 +1,16 @@
 ---
 
 ---
- 
-function myFunction() {
-    var x = document.getElementById("header_spacer").value;
-    document.getElementById("demo").innerHTML = x;
-}
- 
+$(document).ready(function(){
+        $("input[type='button']").click(function(){
+            var radioValue = $("input[name='test']:checked").val();
+            if(radioValue){
+                alert("Your are a - " + radioValue);
+            }
+        });
+        
+    });
+
 $("#header_spacer").change(function(){
 var mc_header_spacer ='{% highlight html %}{% include mc.html %}{% endhighlight %}';
 $('#code-example').html(mc_header_spacer);
