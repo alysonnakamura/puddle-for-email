9d52f753ade72c2ffbc41928c3336eb856d43d5c
alysonnakamura <howdy@alysonnakamura.com>
2017-05-29 11:05:38 -0400

    Update main.js

--- docs/js/main.js
+++ docs/js/main.js
@@ -10,8 +10,3 @@ $(document).ready(function(){
         });
         
     });
-
-$("#header_spacer").change(function(){
-var mc_header_spacer ='{% highlight html %}{% include mc.html %}{% endhighlight %}';
-$('#code-example').html(mc_header_spacer);
-});
