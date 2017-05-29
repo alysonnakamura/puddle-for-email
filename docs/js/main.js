24055a0abff5fe43b6e2f7c62a90faee57786345
alysonnakamura <howdy@alysonnakamura.com>
2017-05-29 11:30:28 -0400

    Update main.js

--- docs/js/main.js
+++ docs/js/main.js
@@ -5,7 +5,7 @@ $(document).ready(function(){
         $("input[type='button']").click(function(){
             var radioValue = $("input[name='test']:checked").val();
             if(radioValue){
-            {% capture component_variable %}{{ radioValue }}{% endcapture %}
+            {% capture component_variable %}{% raw %} radioValue {% endraw %}{% endcapture %}
              }
             {{ component_variable }}
         });
