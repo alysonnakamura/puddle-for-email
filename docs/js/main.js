053af3956ec39ed361bc97f9d88a1e64320f1ac2
alysonnakamura <howdy@alysonnakamura.com>
2017-05-29 10:55:37 -0400

    Update main.js

--- docs/js/main.js
+++ docs/js/main.js
@@ -5,5 +5,5 @@
 $("#header_spacer").change(function(){
 var mc_header_spacer ='{% highlight html %}{% include mc.html %}{% endhighlight %}';
 $('#code-example').html(mc_header_spacer);
-$('input[name=test]:checked').val();
+alert($('input[name=test]:checked').val());
 });
