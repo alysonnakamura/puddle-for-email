a8c8173662ec3de7ff6928c200549dd8443a7706
alysonnakamura <howdy@alysonnakamura.com>
2017-05-29 09:44:26 -0400

    Update main.js

--- docs/js/main.js
+++ docs/js/main.js
@@ -3,7 +3,7 @@ mc_header_spacer: mc_header-spacer.html
 ---
 
 $("#header_spacer").change(function(){
-{% capture component_variable %}$('input[name=test]:checked').val(){% endcapture %}
+{% capture component_variable %} mc_header-spacer.html{% endcapture %}
 var mc_header_spacer ='{% highlight html %}{% include {{ component_variable }} %}{% endhighlight %}';
 $('#code-example').html(mc_header_spacer);
 });
