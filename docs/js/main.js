19828eb50ac870e85772047f168594f9620b224f
alysonnakamura <howdy@alysonnakamura.com>
2017-05-29 13:14:09 -0400

    Update main.js

--- docs/js/main.js
+++ docs/js/main.js
@@ -4,7 +4,7 @@
 $(document).ready(function(){
         $("input[type='button']").click(function(){
             {% capture component_variable %} mc_header-spacer.html {% endcapture %}
-            var post ='{% highlight html %}{% include component_variable %}{% endhighlight %}';
+            var post ='{% highlight html %}{% include { component_variable } %}{% endhighlight %}';
             $('#test').html(post);
         });
         
