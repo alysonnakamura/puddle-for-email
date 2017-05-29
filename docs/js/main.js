fe30975f14775b6e8dd5042cfdef41b1e230ff97
alysonnakamura <howdy@alysonnakamura.com>
2017-05-29 12:06:33 -0400

    Update main.js

--- docs/js/main.js
+++ docs/js/main.js
@@ -4,7 +4,7 @@
 $(document).ready(function(){
         $("input[type='button']").click(function(){
             var component = $("input[name='test']:checked").val();
-            var test = {% capture test_var %}{% raw %}component{% endraw %}{% endcapture %};
+            var test = {% capture test_var %}component{% endcapture %};
             $('#test').html(test);
         });
         
