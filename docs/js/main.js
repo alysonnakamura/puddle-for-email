b24d8a901c20698559d51b8ba02afa7b45d71a5b
alysonnakamura <howdy@alysonnakamura.com>
2017-05-29 11:13:24 -0400

    Update main.js

--- docs/js/main.js
+++ docs/js/main.js
@@ -5,8 +5,9 @@ $(document).ready(function(){
         $("input[type='button']").click(function(){
             var radioValue = $("input[name='test']:checked").val();
             if(radioValue){
-            {% capture component_variable %}radioValue{% endcapture %}
-                                                      }
+            {% capture component_variable %}(radioValue){% endcapture %}
+             }
+            var mc_header_spacer ='{% highlight html %}{% include {{ component_variable }} %}{% endhighlight %}';
         });
         
     });
