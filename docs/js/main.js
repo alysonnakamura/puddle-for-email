57f994bdf0a6344af7ccafa9a33fb58533609daa
alysonnakamura <howdy@alysonnakamura.com>
2017-05-29 12:43:23 -0400

    Update main.js

--- docs/js/main.js
+++ docs/js/main.js
@@ -5,7 +5,7 @@ $(document).ready(function(){
         $("input[type='button']").click(function(){
             var component = $("input[name='test']:checked").val();
             var test = {% capture test_var %}{% raw %}component{% endraw %}{% endcapture %}
-            var post ='{% highlight html %}{% include {{ test_var }} %}{% endhighlight %}'
+            var post ='{% highlight html %}{% include test_var %}{% endhighlight %}'
             $('#test').html(post);
         });
         
