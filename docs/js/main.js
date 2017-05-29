fee2f7934ab931ab07d590e5860c15d24ad36a9e
alysonnakamura <howdy@alysonnakamura.com>
2017-05-29 09:02:55 -0400

    Update main.js

--- docs/js/main.js
+++ docs/js/main.js
@@ -1,1 +1,9 @@
+---
 
+---
+
+	$("#header_spacer").change(function(){
+		    {% capture component_variable %}$('input[name=test]:checked').val(){% endcapture %}
+		var mc_header_spacer ='{% highlight html %}{% include mc.html content=component_variable %}{% endhighlight %}';
+         $('#code-example').html(mc_header_spacer);
+});
