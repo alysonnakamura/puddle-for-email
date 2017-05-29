dd555e067c7951006322954687e833ab2f4fdcb3
alysonnakamura <howdy@alysonnakamura.com>
2017-05-29 11:44:01 -0400

    Update main.js

--- docs/js/main.js
+++ docs/js/main.js
@@ -4,7 +4,8 @@
 $(document).ready(function(){
         $("input[type='button']").click(function(){
             var component = $("input[name='test']:checked").val();
-            $('#test').html({ % raw %} component {% endraw %});
+            var test = { % capture % }{ % raw %} component {% endraw %}{ % endcapture %};
+            $('#test').html(test);
         });
         
     });
