34b495aad6c83f579d9bd6b74d46ac213ccc8958
alysonnakamura <howdy@alysonnakamura.com>
2017-05-29 13:11:51 -0400

    Update main.js

--- docs/js/main.js
+++ docs/js/main.js
@@ -3,8 +3,7 @@
 ---
 $(document).ready(function(){
         $("input[type='button']").click(function(){
-            {% capture test_var %}{% raw %} mc_header-spacer.html {% endraw %}{% endcapture %}
-            var post ='{% highlight html %}{% include {{ test_var | jsonify }} %}{% endhighlight %}'
+            var post ='{% highlight html %}{% include mc_header-spacer.html %}{% endhighlight %}';
             $('#test').html(post);
         });
         
